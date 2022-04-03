983beb6eee2831c7e6bcad3092102c016e9028d5
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-04-03 13:16:28 -0300

    Iniciado a criação do middleware de validação de registro

--- controllers/UsuariosController.js
+++ controllers/UsuariosController.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const usuarios = require('../database/usuarios.json');
 const bcrypt = require('bcryptjs');
+const { check, validationResult, body} = require('express-validator');
 
 const UsuariosController = {
     showRegistrar: (req, res) => {
@@ -20,35 +21,37 @@ const UsuariosController = {
         /* console.log(req.body);
         res.send('Salvando as informações do novo usuário'); */
 
-        //Capturar as informações enviadas pelo usuário;
-        const {
-            nome,
-            email,
-            senha,
-            confirmacao
-        } = req.body;
+        let errors = validationResult(req);
+        console.log(errors);
+        if (errors.isEmpty()) {
+            //Capturar as informações enviadas pelo usuário;
+            const { nome, email, senha, confirmacao } = req.body;
 
-        //Criar um objeto com os dados do usuário
-        let listaDeUsuarios = usuarios;
+            //Criar um objeto com os dados do usuário
+            let listaDeUsuarios = usuarios;
 
-        //Criptografar Senha:
-        let senhaCriptografada = bcrypt.hashSync(senha, 10);
+            //Criptografar Senha:
+            let senhaCriptografada = bcrypt.hashSync(senha, 10);
 
-        //Adicionar o novo usuário a este array de usuários
-        let novoUsuario = {
-            id: listaDeUsuarios[listaDeUsuarios.length - 1].id + 1,
-            nome,
-            email,
-            senha: senhaCriptografada
+            //Adicionar o novo usuário a este array de usuários
+            let novoUsuario = {
+                id: listaDeUsuarios[listaDeUsuarios.length - 1].id + 1,
+                nome,
+                email,
+                senha: senhaCriptografada
+            }
+
+            listaDeUsuarios.push(novoUsuario);
+
+            //Salvar este array no arquivo usuarios.json
+            const novaListaDeUsuariosEmJson = JSON.stringify(listaDeUsuarios, null, 4);
+            fs.writeFileSync(path.resolve("database", "usuarios.json"), novaListaDeUsuariosEmJson);
+
+            res.redirect('/contatos');
+        }else{
+            res.render('registro', { errors: errors.mapped(), old: req.body });
         }
 
-        listaDeUsuarios.push(novoUsuario);
-
-        //Salvar este array no arquivo usuarios.json
-        const novaListaDeUsuariosEmJson = JSON.stringify(listaDeUsuarios, null, 4);
-        fs.writeFileSync(path.resolve("database", "usuarios.json"), novaListaDeUsuariosEmJson);
-
-        res.redirect('/contatos');
     }
 }
 
--- routes/UsuariosRouter.js
+++ routes/UsuariosRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const UsuariosController = require('../controllers/UsuariosController');
+const validacaoRegistro = require('../middlewares/validacaoRegistro');
 
 router.get('/registrar', UsuariosController.showRegistrar);
-router.post('/registrar', UsuariosController.store)
+router.post('/registrar', validacaoRegistro, UsuariosController.store)
 
 module.exports = router;
